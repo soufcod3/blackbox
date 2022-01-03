caefef8f755bd1e817b58c2fa21dcfae77a5939f
Soufiane <soufwra@gmail.com>
2022-01-03 21:11:38 +0100

    route @app_profile

--- assets/app.js
+++ assets/app.js
@@ -14,6 +14,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import './styles/_navbar.scss';
 import './styles/home.scss';
 import './styles/login.scss';
+import './styles/profile.scss';
 
 // start the Stimulus application
 import './bootstrap';
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,5 +17,20 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
+    }
+
+    /**
+     * @Route("/profile", name="app_profile")
+     */
+    public function profile(): Response
+    {
+        $users = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->findAll();
+
+        return $this->render(
+            'home/profile.html.twig',
+            ['users' => $users]
+        );
     }
 }
