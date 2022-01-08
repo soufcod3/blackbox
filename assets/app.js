c0b511ca356bdc77635d749d6e8025c66deb34e7
Soufiane <soufwra@gmail.com>
2022-01-08 13:18:16 +0100

    search form css

--- assets/app.js
+++ assets/app.js
@@ -7,7 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
-import './styles/series.scss';
+import './styles/programs.scss';
 import './styles/fonts.scss';
 import './styles/_footer.scss';
 import 'bootstrap-icons/font/bootstrap-icons.css';
